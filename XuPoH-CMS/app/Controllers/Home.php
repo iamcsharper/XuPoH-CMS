587af8a1c23511d757103513feaaee544f26d9e8
XuPoH <ilyha19@live.ru>
2015-09-12 18:55:21 +0500

    Update Home.php

--- XuPoH-CMS/app/Controllers/Home.php
+++ XuPoH-CMS/app/Controllers/Home.php
@@ -3,13 +3,10 @@ class Home extends AbstractController {
 	public function index() {
 		$config = Core::getSettings();
 		
-		echo '<title>';
-		echo $config->getSiteName();
-		echo '</title>';
-		
-		echo '<h4>Test page!</h4><p>It works! <small>Powered by XuPoH</small></p>';	
-		
-		echo 'Execution time: ' . Core::getExecutionTime();
+		echo $this->view("home", [
+			"testVar" => "testVal",
+			"config" => $config
+		]);
 	}
 }
 
