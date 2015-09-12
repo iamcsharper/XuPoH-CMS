f3f2a0b39bde32d0ef303c4bdac238eee9a6ccd1
XuPoH <ilyha19@live.ru>
2015-09-12 11:17:31 +0500

    Update URLParser.php

--- XuPoH-CMS/engine/Helpers/URLParser.php
+++ XuPoH-CMS/engine/Helpers/URLParser.php
@@ -3,17 +3,24 @@ class URLParser {
 	private $url;
 	public function __construct() {
 		$this->url = $this->parseUrl();
-	}		public function getURL() {		return $this->url;
 	}
+	
+	public function getURL() {
+		return $this->url;
+	}
+	
 	public function getControllerName() {
 		return $this->url[0];
 	}
+	
 	public function getControllerMethod() {
 		return $this->url[1];
 	}
+	
 	public function getControllerParams() {
 		return $this->url[3] ? array_values($this->url) : [];
 	}
+	
 	private function parseUrl() {
 		if (isset($_GET['url'])) {
 			return explode('/', rtrim($_GET['url'], '/'));
